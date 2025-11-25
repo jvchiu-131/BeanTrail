6709385590eb7fe6d0353dbbb038fb3f3cc3f4ba
jvchiu-131 <jvchiu131@gmail.com>
2025-11-25 09:55:30 +0800

    fix: adjusted the main.ts cors allowed origins to allow the vercel app to access the api

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    
+  
+    'https://beantrail.vercel.app', 
+    
+      //for local deployment testing
+    'http://localhost:3000',
+  ];
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
